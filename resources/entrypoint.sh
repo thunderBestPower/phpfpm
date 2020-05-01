657a81b2665d1e5748ed1f12c3dc08509f6cb2a2
Mattia Vitali <vitali.mattia@gmail.com>
2020-05-01 09:17:53 +0200

    feat: default value Max_upload_size e Post_max_size

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -3,6 +3,8 @@
 sed -i -e "s/REMOTE_HOST/${DOCKER_HOST_IP}/" /usr/local/etc/php/conf.d/php.ini
 sed -i -e "s/REMOTE_PORT/${DOCKER_HOST_PORT}/" /usr/local/etc/php/conf.d/php.ini
 sed -i -e "s/ENABLE_XDEBUG/${ENABLE_XDEBUG}/" /usr/local/etc/php/conf.d/php.ini
+sed -i -e "s/MAX_UPLOAD_SIZE/${MAX_UPLOAD_SIZE}/" /usr/local/etc/php/conf.d/php.ini
+sed -i -e "s/POST_MAX_SIZE/${POST_MAX_SIZE}/" /usr/local/etc/php/conf.d/php.ini
 
 if [ 0 == "${ENABLE_XDEBUG}" ]; then
     if [ -e /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini ]; then
